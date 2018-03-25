a930f70c28c137d6b656f8ed3e4f4ad0ec5a3299
Ryuji Ganaha <ryujignh@gmail.com>
2018-03-26 08:19:46 +0900

    Implementing image detect function.

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -27,9 +27,41 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         if let userPickedimage = info[UIImagePickerControllerOriginalImage] as? UIImage {
             imageView.image = userPickedimage
+            
+            guard let ciimage = CIImage(image: userPickedimage) else {
+                fatalError("Converting image failed")
+            }
+            
+            detect(image: ciimage)
+            
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CoreML model failed")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image")
+            }
+            
+            print(results)
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try! handler.perform([request])
+        }
+        catch {
+            print(error)
+        }
         
     }
 
